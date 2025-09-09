f1d287c5d2143241afd69561248fba5824fb0c65
gavinmonteiro <gtmontei@uvm.edu>
2025-09-09 13:02:46 -0400

    Made Deck.h and Deck.cpp

---
+++ Deck.h
@@ -0,0 +1,8 @@
+//
+// Created by Gavin  Monteiro on 9/9/25.
+//
+
+#ifndef M1OEP_GTMONTEI_DECK_H
+#define M1OEP_GTMONTEI_DECK_H
+
+#endif //M1OEP_GTMONTEI_DECK_H
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# M1OEP-gtmontei
+# CS 2300 Modulee 1 Open Ended Project:
+
+Brainstorming ideas
+- blackjack
+- farm organizer
+- war
+- 
