814a6e5d0a8ca0dc01f467b516577a746b7849d2
gavinmonteiro <gtmontei@uvm.edu>
2025-09-11 17:25:08 -0400

    Working on Dealer and Blackjack.cpp

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,5 +1,28 @@
 //
 // Created by Gavin  Monteiro on 9/9/25.
 //
+#include "Dealer.h"
 
 /************* Dealer class ******************/
+
+Dealer::Dealer() {
+}
+
+
+void Dealer::setHand(Card card1, Card card2) {
+    // In case anything is in the hand, clears it
+    hand.clear();
+    // Adds first card
+    hand.push_back(card1);
+    // Adds second card
+    hand.push_back(card2);
+}
+
+vector<Card> Dealer::getHand(){
+    return hand;
+}
+
+void Dealer::addCard(Card card) {
+    // Adds new card to hand vector
+    hand.push_back(card);
+}
